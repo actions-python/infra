2ce92234423cb36887c4c0f354908bc8e0df51f4
sudosubin <sudosubin@gmail.com>
2023-10-13 17:32:46 +0900

    chore: Update variables order to trigger tfcmt

--- github/variables.tf
+++ github/variables.tf
@@ -1,3 +1,9 @@
+variable "app_terraform_io_token" {
+  type        = string
+  sensitive   = true
+  description = "Terraform Cloud api token for organization"
+}
+
 variable "github_provider_app_id" {
   type        = string
   sensitive   = true
@@ -14,10 +20,4 @@ variable "github_provider_app_pem_file" {
   type        = string
   sensitive   = true
   description = "GitHub App private key PEM file content"
-}
-
-variable "app_terraform_io_token" {
-  type        = string
-  sensitive   = true
-  description = "Terraform Cloud api token for organization"
 }
