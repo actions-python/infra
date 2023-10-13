2c53aa0f1fbee04e753aa4494e550f7b589f28aa
sudosubin <sudosubin@gmail.com>
2023-10-14 02:55:37 +0900

    chore: Remove unused tfe organization token

--- terraform-cloud/nested_organization.tf
+++ terraform-cloud/nested_organization.tf
@@ -2,7 +2,3 @@ resource "tfe_organization" "actions_python" {
   name  = "actions-python"
   email = "sudosubin@gmail.com"
 }
-
-resource "tfe_organization_token" "actions_python" {
-  organization = tfe_organization.actions_python.name
-}
